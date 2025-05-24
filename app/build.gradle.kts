ec70f7d11cd0c07cf77089b287caad472da1cbb0
nbazo06 <23520117@gm.uit.edu.vn>
2025-05-24 15:51:54 +0700

    homescreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.compose.material:material-icons-extended:1.6.0")
+    implementation ("androidx.navigation:navigation-compose:2.7.0")
 }
