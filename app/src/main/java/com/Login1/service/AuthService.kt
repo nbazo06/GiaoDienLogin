84ef361ddd24e6764ac8445822cc4f82b5a44dec
Phúc Tăng <tangphuc0103@gmail.com>
2025-06-20 12:43:30 +0700

    add more icon

--- app/src/main/java/com/Login1/service/AuthService.kt
+++ app/src/main/java/com/Login1/service/AuthService.kt
@@ -362,6 +362,35 @@ class AuthService {
             }
         }
 
+        suspend fun getNotifications(accountId: String): Result<List<NotificationItem>> {
+            return try {
+                val response = get("$BASE_URL/notifications/$accountId")
+                if (response.getString("status") == "success") {
+                    val notificationsJsonArray = response.getJSONArray("notifications")
+                    val notifications = (0 until notificationsJsonArray.length()).map { i ->
+                        val item = notificationsJsonArray.getJSONObject(i)
+                        NotificationItem(
+                            notificationID = item.getInt("notification_id"),
+                            accountID = accountId.toInt(),
+                            userID = 0, // nếu backend không trả thì có thể mặc định là 0 hoặc bỏ luôn
+                            title = item.getString("title"),
+                            message = item.getString("message"),
+                            type = item.getString("type"),
+                            isRead = item.getBoolean("is_read"),
+                            sentAt = item.getString("sent_at")
+                        )
+                    }
+                    Result.success(notifications)
+                } else {
+                    Result.failure(Exception("Không lấy được danh sách thông báo"))
+                }
+            } catch (e: Exception) {
+                Log.e("AuthService", "Lỗi lấy notifications: ${e.message}", e)
+                Result.failure(e)
+            }
+        }
+
+
         suspend fun getCategories(userId: String): Result<List<Category>> {
             Log.d("AuthService", "Calling /categories API with userId: $userId")
             return try {
